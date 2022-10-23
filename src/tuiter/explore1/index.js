1d2ad945af5f4019398410edb08735caf1fc7561
DESKTOP-88L6VBB\91903 <hkvin4@gmail.com>
2022-10-23 13:42:13 -0400

    a6

--- src/tuiter/explore1/index.js
+++ src/tuiter/explore1/index.js
@@ -19,13 +19,13 @@ const ExploreComponent = () => {
      </div>
      <ul className="nav nav-pills mb-2 mt-1">
        <li className="nav-item">
-         <a className="nav-link active">For You</a>
+         <a className="nav-link active" href="#">For You</a>
        </li>
        <li className="nav-item">
-         <a className="nav-link">Trending</a>
+         <a className="nav-link" href="#">Trending</a>
        </li>
        <li className="nav-item">
-         <a className="nav-link">News</a>
+         <a className="nav-link" href="#">News</a>
        </li>
      </ul>
      <div className="position-relative mb-2">
