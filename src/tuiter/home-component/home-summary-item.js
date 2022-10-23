e0aa5ae9cfc04f7d99928f879d638cbdf1012b2d
DESKTOP-88L6VBB\91903 <hkvin4@gmail.com>
2022-10-23 13:10:40 -0400

    a6

--- src/tuiter/home-component/home-summary-item.js
+++ src/tuiter/home-component/home-summary-item.js
@@ -6,43 +6,43 @@ const PostSummaryItem1 = (
 ) => {
  return(
   <li className="list-group-item">
-   <div class="row">
-     <div class="col-1 ps-0 ">
+   <div className="row">
+     <div className="col-1 ps-0 ">
           <img className="float-left float-start rounded-circle" src={`/images/${post.profilepic}`} height ={45} width={45}/>
      </div>
-     <div class="col-11">
-        <div class ="text-muted">{post.retweet}</div>
-        <div class=" fw-bold">{post.userName} <i class="text-primary bi bi-check-circle-fill"/> <span class="text-muted"> {post.handle} - {post.time} </span> </div>
-        <div class="">{post.post}</div>
+     <div className="col-11">
+        <div className ="text-muted">{post.retweet}</div>
+        <div className=" fw-bold">{post.userName} <i className="text-primary bi bi-check-circle-fill"/> <span className="text-muted"> {post.handle} - {post.time} </span> </div>
+        <div className="">{post.post}</div>
               
-              <div class="mt-2 border border-secondary rounded">
+              <div className="mt-2 border border-secondary rounded">
           
-              <img class=" w-100 border-bottom border-secondary rounded "  src={`/images/${post.image}`} height = {340}/>
+              <img className=" w-100 border-bottom border-secondary rounded "  src={`/images/${post.image}`} height = {340}/>
               
             
                 
         </div>
 
 
-        <ul class="nav   align-items-center nav-justified mt-2 p-0">
-          <li class="nav-item">
-            <a class="nav-link text-muted" href="#"><i class="bi bi-chat text-muted"></i>  {post.comments}</a>
+        <ul className="nav   align-items-center nav-justified mt-2 p-0">
+          <li className="nav-item">
+            <a className="nav-link text-muted" href="#"><i className="bi bi-chat text-muted"></i>  {post.comments}</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-muted" href="#"><i class="bi bi-heart text-muted"></i> {post.retuits}</a>
+          <li className="nav-item">
+            <a className="nav-link text-muted" href="#"><i className="bi bi-heart text-muted"></i> {post.retuits}</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-muted" href="#"><i class="bi bi-arrow-repeat text-muted"></i> {post.likes}</a>
+          <li className="nav-item">
+            <a className="nav-link text-muted" href="#"><i className="bi bi-arrow-repeat text-muted"></i> {post.likes}</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-muted " href="#"><i class="bi bi-box-arrow-up text-muted"></i></a>
+          <li className="nav-item">
+            <a className="nav-link text-muted " href="#"><i className="bi bi-box-arrow-up text-muted"></i></a>
           </li>
           
           
      </ul>
 
          <div >
-         <a class="fw-bold text-primary text-decoration-none" href="#"> {post.show} </a> 
+         <a className="fw-bold text-primary text-decoration-none" href="#"> {post.show} </a> 
         </div>
     </div>
    </div>
